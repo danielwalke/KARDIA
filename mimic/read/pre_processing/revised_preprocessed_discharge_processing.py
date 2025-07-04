75c13eb630a6b53ee2564c9a1afa7434ce8c5337
danielwalke <daniel-walke@t-online.de>
2025-07-04 08:05:34 +0200

    new KG creation scripts and experiments with more KGs

--- mimic/read/pre_processing/revised_preprocessed_discharge_processing.py
+++ mimic/read/pre_processing/revised_preprocessed_discharge_processing.py
@@ -22,8 +22,12 @@ session = Session()
 discharge_notes = session.query(RevisedNote).all()
 new_notes = []
 for note in tqdm(discharge_notes):
+	print(note.text)
 	new_note = process_text(note.text)
 	new_notes.append(PreprocessedRevisedNote(row_id = note.row_id, note_id = note.note_id, subject_id = note.subject_id, hadm_id=note.hadm_id, charttime=note.charttime, text=new_note))
+	print(new_note)
+	break
+exit()
 with Session() as session:
 	session.add_all(new_notes)
 	session.commit()
